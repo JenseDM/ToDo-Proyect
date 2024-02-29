3795cdc77693afa0ebe132dd695af488d068e300
Jense <jense.martinez@utp.edu.co>
2024-02-29 18:20:27 -0500

    Version 1.0
    ALL ALREADY

--- tasks/models.py
+++ tasks/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
+#Project model
 class Project(models.Model):
     title = models.CharField(max_length=50)
     datecrated = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    
+    #The __str__ method is used to display the title of the project in the admin panel
     def __str__(self):
         return self.title + '- by ' + self.user.username
-    
+#Task model
 class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
@@ -19,5 +19,6 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
+    #The __str__ method is used to display the title of the task in the admin panel
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.db import IntegrityError
-from django.shortcuts import render, redirect,get_object_or_404, HttpResponse
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -15,7 +15,7 @@ def home(request):
 
 # View for the signup page
 def signup(request):
-
+    # GET request
     if request.method == 'GET':
         return render(request, 'signup.html', {
         'form': UserCreationForm()
@@ -39,32 +39,22 @@ def signup(request):
                    'error': 'Passwords did not match'
                    })
 
-@login_required
-def projects(request):
-    
-    projects = Project.objects.filter(user=request.user, completed=False)
-    if projects.count() == 0:
-        return render(request, 'projects.html', {
-            'message': 'You have not created any projects yet.'
-        })
-    return render(request, 'projects.html', {
-        'projects': projects,
-        'message': 'Your projects are here.'
-    }) 
-
+# View for the signout page
 @login_required
 def signout(request):
+    # POST request
     logout(request)
     return redirect('home')   
 
-
+# View for the signin page
 def signin(request):
-
+    # GET request
     if request.method == 'GET':
         return render(request, 'signin.html', {
         'form': AuthenticationForm()
     })
     else:
+        # POST request
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {
@@ -72,18 +62,22 @@ def signin(request):
                 'error': 'Username or password is incorrect'
             })
         else:
+            # Login user
             login(request, user)
             return redirect('projects')
-        
+
+# View for the project form page
 @login_required
 def createProject(request):
-
+    # GET request
     if request.method == 'GET':
         return render(request, 'createProject.html', {
             'form': ProjectForm
         })
     else:
+        # POST request
         try:
+            # Create project
             form =  ProjectForm(request.POST)
             newProject = form.save(commit=False)
             newProject.user = request.user
@@ -94,12 +88,53 @@ def createProject(request):
                 'form': ProjectForm(),
                 'error': 'Please provide valid data, try again.'
             })
-        
+
+# View for the projects page
+@login_required
+def projects(request):
+    # GET request
+    projects = Project.objects.filter(user=request.user, completed=False)
+    if projects.count() == 0:
+        return render(request, 'projects.html', {
+            'message': 'There are no pending projects.'
+        })
+    return render(request, 'projects.html', {
+        'projects': projects,
+        'message': 'Your projects are here.'
+    }) 
+
+# View for the create task form page
+@login_required
+def createTask(request, project_id):
+    # GET request
+    if request.method == 'GET':
+        return render(request, 'createTask.html', {
+            'form': TaskForm,
+        })
+    else:
+        # POST request
+        try:
+            # Create task
+            form =  TaskForm(request.POST)
+            newTask = form.save(commit=False)
+            newTask.project = Project.objects.get(pk=project_id)
+            newTask.save()
+            return redirect('tasks', project_id=project_id)
+        except ValueError:
+            return render(request, 'createTask.html', {
+                'form': TaskForm(),
+                'error': 'Please provide valid data, try again.'
+            })
+
+# View for the tasks page
 @login_required
 def tasks(request, project_id):
+    # GET request
     project = get_object_or_404(Project, pk=project_id)
     tasks = Task.objects.filter(project=project_id, completed=False)
+    
     if tasks.count() == 0:
+        # If there are no tasks
         return render(request, 'tasks.html', {
             'message': 'You have not created any tasks yet.',
             'project_id': project_id,
@@ -111,55 +146,49 @@ def tasks(request, project_id):
         'project': project
     })        
 
+# View for the task detail page
 @login_required
 def taskDetail(request, idtask):
+    # GET request
     task = get_object_or_404(Task, pk=idtask)
     return render(request, 'taskDetail.html', {
         'task': task
     })
 
+# View for complete task
 @login_required
-def createTask(request, project_id):
-
-    if request.method == 'GET':
-        return render(request, 'createTask.html', {
-            'form': TaskForm,
-        })
-    else:
-        try:
-            form =  TaskForm(request.POST)
-            newTask = form.save(commit=False)
-            newTask.project = Project.objects.get(pk=project_id)
-            newTask.save()
-            return redirect('tasks', project_id=project_id)
-        except ValueError:
-            return render(request, 'createTask.html', {
-                'form': TaskForm(),
-                'error': 'Please provide valid data, try again.'
-            })
-        
-@login_required
-def completeTask(request, idtask):
+def completeTask(request, idtask): 
    task =  get_object_or_404(Task, pk=idtask)
    if request.method == 'POST':
        task.completed = True
        task.save()
        finished_tasks = Task.objects.filter(project=task.project, completed=True).count()
        all_tasks = Task.objects.filter(project=task.project).count()
-
+        # If all tasks are completed, the project is completed
        if finished_tasks == all_tasks:
             project = Project.objects.get(pk=task.project.id)
             project.completed = True
-            project.save()
+            project.delete()
+            return redirect('projects')
        return redirect('tasks', project_id = task.project.id)
-   
+
+# View for delete task
 @login_required   
 def deleteTask(request, idtask):
     task =  get_object_or_404(Task, pk=idtask)
     if request.method == 'POST':
          task.delete()
+         finished_tasks = Task.objects.filter(project=task.project, completed=True).count()
+         all_tasks = Task.objects.filter(project=task.project).count()
+         # If all tasks are completed, the project is completed
+         if finished_tasks == all_tasks:
+            project = Project.objects.get(pk=task.project.id)
+            project.completed = True
+            project.delete()
+            return redirect('projects')
          return redirect('tasks', project_id = task.project.id)
-    
+
+# View for delete project
 @login_required
 def deleteProject(request, project_id):
     project =  get_object_or_404(Project, pk=project_id)
